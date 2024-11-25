078e6c7baa8cd39be19ed1b849ee767cfda7ea21
mohamed barakat <mohmedbarakat242@gmail.com>
2024-11-25 14:54:18 +0200

    starting from here masoad

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:get_storage/get_storage.dart';
 
 import 'package:store_app_connectx/app.dart';
+import 'package:store_app_connectx/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   final WidgetsBinding widgetsBinding =
@@ -9,8 +12,8 @@ Future<void> main() async {
 
   await GetStorage.init();
 
-  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
-  //     .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
 
   runApp(const App());
 }
